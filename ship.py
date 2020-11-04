f460f77b88eb5dd44e0be2956747ad5975dddda6
tth <tzehon.tan@gmail.com>
2020-11-04 16:30:59 +0800

    Move ship left

--- ship.py
+++ ship.py
@@ -15,13 +15,16 @@ class Ship:
         # Start each new ship at the bottom centre of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
 
-        # Movement flag
+        # Movement flags
         self.moving_right = False
+        self.moving_left = False
   
     def update(self):
         """Update the ship's position based on the movement flag."""
         if self.moving_right:
             self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
 
     def blitme(self):
         """Draw the ship at its current location."""
