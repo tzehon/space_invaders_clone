fe8a56741c3271250cdbc057433670d8a688411d
tth <tzehon.tan@gmail.com>
2021-01-17 13:42:32 +0800

    Display ship lives

--- ship.py
+++ ship.py
@@ -1,10 +1,12 @@
 import pygame
+from pygame.sprite import Sprite
 
-class Ship:
+class Ship(Sprite):
     """A class to manage the ship."""
 
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
+        super().__init__()
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
         self.settings = ai_game.settings
@@ -22,7 +24,7 @@ class Ship:
         # Movement flags
         self.moving_right = False
         self.moving_left = False
-  
+
     def update(self):
         """Update the ship's position based on the movement flag."""
         # Update the ship's x value, not the rect
