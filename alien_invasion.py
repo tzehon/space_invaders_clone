0f82b9a25870173fe1f54db4c9747e400cfb7266
tth <tzehon.tan@gmail.com>
2021-01-17 11:39:19 +0800

    Reset the game when it ends

--- alien_invasion.py
+++ alien_invasion.py
@@ -160,7 +160,17 @@ class AlienInvasion:
     def _check_play_button(self, mouse_pos):
         """Start a new game when the player clicks Play."""
         if self.play_button.rect.collidepoint(mouse_pos):
+            # Reset the game statistics.
+            self.stats.reset_stats()
             self.stats.game_active = True
+
+            # Get rid of any remaining aliens and bullets.
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # Create a new fleet and center the ship.
+            self._create_fleet()
+            self.ship.centre_ship()
 
     def _check_keyup_events(self, event):
         """Respond to key releases."""
